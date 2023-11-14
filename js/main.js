13d05555144c7f0f51769f3a40a2398b55611828
Paxton Rosenberg <rosenbergpaxton@gmail.om>
2023-11-14 23:17:14 +0000

    Remove the example HTML entries from index.html once the user's entries are successfully being appended to the DOM on page load and replace them with a message indicating that no entries have been recorded which matches the figma.

--- js/main.js
+++ js/main.js
@@ -34,10 +34,33 @@ $form.addEventListener('submit', function (event) {
 });
 
 function renderEntry(entry) {
-  const $listItem = document.querySelector('li');
+  const $listItem = document.createElement('li');
+  $listItem.setAttribute('class', 'list-display');
+
+  const $columnHalf = document.createElement('div');
+  $columnHalf.setAttribute('class', 'column-half');
+  $listItem.appendChild($columnHalf);
+
+  const $image = document.createElement('img');
+  $image.setAttribute('src', entry.url);
+  $columnHalf.appendChild($image);
+
+  const $columnHalf2 = document.createElement('div');
+  $columnHalf2.setAttribute('class', 'column-half');
+  $listItem.append($columnHalf2);
+
+  const $title = document.createElement('h2');
+  $title.textContent = entry.title;
+  $columnHalf2.append($title);
+
+  const $notes = document.createElement('p');
+  $notes.textContent = entry.notes;
+  $columnHalf2.append($notes);
 
   return $listItem;
 }
+
+renderEntry(data.entries[0]);
 
 const $ul = document.querySelector('ul');
 
